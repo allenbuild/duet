cb76a6ffd34bf8dc58c75b2e8dfcd8d439d71f06
Allen Xu <allenxu@ist2-01690.apn.wlan.private.upenn.edu>
2026-09-23 13:26:10 -0400

    Update Site source

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ export const metadata: Metadata = {
     canonical: '/',
   },
   icons: {
-    icon: [{ url: '/duet-favicon.png?v=2', type: 'image/png', sizes: '1254x1254' }],
-    shortcut: '/duet-favicon.png?v=2',
+    icon: [{ url: '/duet-favicon.png?v=3', type: 'image/png', sizes: '1254x1254' }],
+    shortcut: '/duet-favicon.png?v=3',
   },
   openGraph: {
     title: 'Duet Labs',
